70fd94a5b51451dc0464d695c7296a8d0f0a50b0
songjilong <2606587750@qq.com>
2020-02-14 14:11:46 +0800

    add bootstrap

--- src/main/java/com/sjl/community/controller/HelloController.java
+++ src/main/java/com/sjl/community/controller/HelloController.java
@@ -1,9 +1,7 @@
 package com.sjl.community.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * @author song
@@ -12,9 +10,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HelloController {
 
-    @GetMapping("/hello")
-    public String hello(@RequestParam(value = "username", required = false, defaultValue = "张三") String username, Model model) {
-        model.addAttribute("username", username);
+    @GetMapping("/")
+    public String index() {
         return "index";
     }
 }
