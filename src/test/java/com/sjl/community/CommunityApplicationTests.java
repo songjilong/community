91418e0cae40836f92def0cf6f1b80dc551edbfd
songjilong <2606587750@qq.com>
2020-04-15 16:14:59 +0800

    添加聊天室

--- src/test/java/com/sjl/community/CommunityApplicationTests.java
+++ src/test/java/com/sjl/community/CommunityApplicationTests.java
@@ -1,13 +1,29 @@
 package com.sjl.community;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
 
 @SpringBootTest
 class CommunityApplicationTests {
 
     @Test
     void contextLoads() {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .get()
+                .url("http://open.iciba.com/dsapi")
+                .build();
+        try (Response response = client.newCall(request).execute()){
+            String string = response.body().string();
+            System.out.println(string);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
